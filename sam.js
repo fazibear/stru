5fea0d06959534867c5735c53a3f8428f353d532
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-22 21:56:16 +0100

    add sam.js

---
+++ sam.js
@@ -0,0 +1,26 @@
+(function(){
+  import('https://unpkg.com/sam-js');
+  function createSamSpeechDataUrl(text, options = {}) {
+    const sam = new SamJs({
+      singmode: false,
+      pitch: 32,
+      speed: 70,
+      mouth: 145,
+      throat: 145,
+      ...options,
+    });
+
+    const wav = sam.wav(text);
+    const part = new Uint8Array(wav);
+    return URL.createObjectURL(new Blob([part],{type:'audio/wav'}));
+  }
+  async function sam(sentences, options) {
+    const urlMap = {};
+    for(const name in sentences) {
+      logger('[sam] Creating '+name+' with: '+sentences[name]);
+      urlMap[name] = createSamSpeechDataUrl(sentences[name],options);
+    }
+    await samples(urlMap);
+  }
+  window.sam = sam;
+})();
