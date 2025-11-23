0ccfbd742bb9c522482f499f04be6191f75848a6
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-23 11:55:15 +0100

    await for import

--- trancegate.js
+++ trancegate.js
@@ -1,5 +1,5 @@
 // taken from https://github.com/switchangel/strudel-scripts/blob/main/allscripts.js
-import('https://stru.fazibear.me/fill.js')
+await import('https://stru.fazibear.me/fill.js')
 register('trancegate', (density, seed, length, x) => {
   return x.struct(rand.mul(density).round().seg(16).rib(seed, length)).fill().clip(.7)
 })
